6f9fada73ef8d746fb65d334395e366ccf964126
Andrey Chernykh <andreychernykh04@gmail.com>
2025-04-21 11:02:29 +0300

    delete comments

--- src/config.py
+++ src/config.py
@@ -5,12 +5,11 @@ Configuration settings for the application
 
 import logging
 import warnings
+
 from bs4 import BeautifulSoup
 
-# API Key for MWS GPT
-API_KEY = "sk-KNo006G2a48UVE3IxFlQEQ"  # REPLACE WITH YOUR KEY
+API_KEY = "sk-KNo006G2a48UVE3IxFlQEQ"
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 logger.handlers = []
@@ -18,10 +17,8 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(console_handler)
 
-# Игнорируем предупреждение RuntimeWarning от BeautifulSoup
 warnings.filterwarnings("ignore", category=RuntimeWarning, module="bs4")
 
-# Default paths
 DEFAULT_INDEX_PATH = "faiss_index.bin"
 DEFAULT_KNOWLEDGE_BASE_PATH = "knowledge_base_with_b2c.csv"
 DEFAULT_PROCESSED_JSON_PATH = "data/processed_articles.json"
--- src/stt_service/config.py
+++ src/stt_service/config.py
@@ -1,38 +1,36 @@
 # logging
-LOGGING_LEVEL = "WARNING" # DEBUG < INFO < WARNING < ERROR < CRITICAL, будет игнорировать все логи уровнем ниже
+LOGGING_LEVEL = "WARNING"  # DEBUG < INFO < WARNING < ERROR < CRITICAL
 WRITE_TO_FILE = False
 LOG_FILE_PATH = "log.log"
-LOG_FILE_MODE = "w" # w - перезаписать, a - добавить 
+LOG_FILE_MODE = "w"  # w - перезаписать, a - добавить
 
 # audio
 SAMPLE_RATE = 16000
-BLOCK_DURATION_MS = 100 # Длительность блока аудио для обработки (в миллисекундах)
-BLOCK_SIZE = int(SAMPLE_RATE * BLOCK_DURATION_MS / 1000) # Размер блока в сэмплах
-CHANNELS = 1 # Моно 
+BLOCK_DURATION_MS = 100  # Длительность блока аудио для обработки (в миллисекундах)
+BLOCK_SIZE = int(SAMPLE_RATE * BLOCK_DURATION_MS / 1000)  # Размер блока в сэмплах
+CHANNELS = 1  # Моно
 
 # whisper
 MODEL_SIZE = "small"  # "tiny", "base", "small", "medium", "large-v1", "large-v2", "large-v3"
-LANGUAGE = "ru"         
-DEVICE = "auto" # cpu, cuda, auto       
-COMPUTE_TYPE = "auto" # int8, float32, auto
+LANGUAGE = "ru"
+DEVICE = "auto"  # cpu, cuda, auto
+COMPUTE_TYPE = "auto"  # int8, float32, auto
 CPU_THREADS = 6
 NUM_WORKERS = 6
 
 # silero VAD
-SILERO_VAD_MODEL = 'silero_vad' # Модель для обработки тишины и шума в потоке
+SILERO_VAD_MODEL = 'silero_vad'  # Модель для обработки тишины и шума в потоке
 SILERO_VAD_REPO = 'snakers4/silero-vad'
-VAD_PROCESS_INTERVAL_SEC = 0.01 # Интервал, через который проверяем буфер с помощью VAD
+VAD_PROCESS_INTERVAL_SEC = 0.01  # Интервал, через который проверяем буфер с помощью VAD
 VAD_PARAMETERS = dict(min_silence_duration_ms=1000, threshold=0.8)
-# threshold: Порог вероятности (чем выше, тем строже VAD)
-# min_silence_duration_ms: Минимальная длительность тишины для разделения сегментов 
 
 # silero TTS & warming up models
 WARMUP_ENABLE = True
 NUM_WARMUP_RUNS = 3
 TTS_NUM_SPEAKERS = 3
-TTS_SILERO_MODEL = 'silero_tts' 
+TTS_SILERO_MODEL = 'silero_tts'
 TTS_SILERO_REPO = 'snakers4/silero-models'
-TTS_SPEAKER = "random" # см. документацию по silero  
+TTS_SPEAKER = "random"  # см. документацию по silero
 TTS_SAMPLE_RATE = 8000
-TTS_MODEL_VERSION = "v4_ru"   
+TTS_MODEL_VERSION = "v4_ru"
 TTS_WARMUP_TEXT = "Это текст для прогрева моделей? Невероятно! Это действительно текст для прогрева моделей."
